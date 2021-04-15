23e5e3b6b8d2190fe251f444f12dc38094465110
Samiur-UTI <allpraisethelord@gmail.com>
2021-04-15 23:39:12 +0600

    responsive init

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import PageHeader from './page/pageheader/pageheader.page';
 import TextInput from './components/textInput/textinput.component';
-import {Grid,makeStyles} from '@material-ui/core';
+import {Grid,makeStyles,CssBaseline} from '@material-ui/core';
 import './App.css';
 import logo from './logo.jpg';
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    padding: '160px 60px 0',
+    width: '100%'
   }
 }));
 function App() {
   const classes = useStyles();
   return (
-    <div className='main'>
-      <PageHeader/>
-      <img className='logo' alt='' src={logo}/>
-          <Grid className={classes.root} container spacing={1}>
-            <Grid item xs={12} sm={6}>
-              <TextInput/>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextInput/>
-            </Grid> 
-          </Grid>
-    </div>
+    <React.Fragment>
+       <CssBaseline />
+      <div className='main'>
+        <PageHeader/>
+        <img className='logo' alt='' src={logo}/>
+              <form>
+                <Grid className={classes.root} container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <TextInput/>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextInput/>
+                  </Grid> 
+                </Grid> 
+              </form>
+        </div>  
+    </React.Fragment>
+    
   );
 }
 
--- src/components/textInput/textinput.component.jsx
+++ src/components/textInput/textinput.component.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import {FormControl,TextField,makeStyles} from "@material-ui/core";
 const useStyles = makeStyles((theme) => ({
     textField: {
-      marginLeft: theme.spacing(2),
-      marginRight: theme.spacing(1),
-      marginTop:theme.spacing(15),
-      width: '90ch',
-      
+        
     },
   }));
   
@@ -14,8 +10,8 @@ export default function TextInput() {
     const classes = useStyles();
     return (
         <div>
-            <FormControl>
-                <TextField className={classes.textField} label="Website" variant="outlined" />
+            <FormControl style={{width: '100%'}}>
+                <TextField fullWidth className={classes.textField} label="Website" variant="outlined" />
             </FormControl>
         </div>
     )
