8f83e0e7c76d6ca610edfbe0c30afa101655bc6c
Samiur-UTI <allpraisethelord@gmail.com>
2021-04-15 12:59:01 +0600

    image stuck

--- src/components/textInput/textinput.component.jsx
+++ src/components/textInput/textinput.component.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import {FormControl,InputLabel,TextField,FormHelperText} from "@material-ui/core";
+import {FormControl,TextField,FormHelperText} from "@material-ui/core";
 export default function TextInput() {
     return (
         <div>
             <FormControl>
-                <InputLabel htmlFor="my-input">Email address</InputLabel>
-                <TextField label="Outlined" variant="outlined" />
+                <TextField label="Website" variant="outlined" />
                 <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
             </FormControl>
         </div>
